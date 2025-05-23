8e8e940cbefdf8fd4abf285d89ac61108fcb87a3
Meydson Baracho <64757536+meydson@users.noreply.github.com>
2025-05-23 16:50:05 -0300

    adicionando a api

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import { api } from "@/services/api"
 
 export default function Home() {
+    const [categories, setCategories] = useState()
+
+    async function fetchCategories() {
+        try {
+            const { data } = await api.get("/categories")
+                console.log(data)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Categorias", "Não foi possível carregar as categorias")
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories()
+    }, [])
+
     return (
         <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
             <Text>Home</Text>
